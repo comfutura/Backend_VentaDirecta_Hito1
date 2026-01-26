fc35bd0ef161200099bc19bcaec7d965a6bcd2c2
JoseEm26 <joseangelespinozamorales@gmail.com>
2026-01-26 11:22:37 -0500

    logo comfutura

--- frontend/src/environment.ts
+++ frontend/src/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://backend-production-9272.up.railway.app',
-  //baseUrl:'http://localhost:8080',
+  //baseUrl: 'https://backend-production-9272.up.railway.app',
+  baseUrl:'http://localhost:8080',
 };
